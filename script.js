f1c9d46383f46e75049fadce3e8ce138a1f22980
Alexander G. Yang <spark8402@rogers.com>
2019-07-28 01:33:48 -0400

    fixed paragraphs format and added some animation effects

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ myFunction = () => {
       x.style.display = "block";
     }
   }
-
-
-
-
+  
   const typed = new Typed('#typed', {
     strings: ["a Full-Stack Web Developer", "a Jiu-Jitsu Black Belt", "a video game enthusiast", "a big Raptors fan", "based in Toronto"],
     typeSpeed: 100,
