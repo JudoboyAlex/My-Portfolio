35e57c6bf99b9c342577174fbbf186e5d8e27915
Alexander G. Yang <spark8402@rogers.com>
2022-01-30 15:51:51 -0500

    updated title and date

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ myFunction = () => {
   }
   
   const typed = new Typed('#typed', {
-    strings: ["a Full-Stack Web Developer", "a Jiu-Jitsu Black Belt", "a video game enthusiast", "a big Raptors fan", "based in Toronto"],
+    strings: ["a Frontend Developer", "a Jiu-Jitsu Black Belt", "a video game enthusiast", "a big Raptors fan", "based in Toronto"],
     typeSpeed: 100,
     backSpeed: 30,
     smartBackspace: true, 
